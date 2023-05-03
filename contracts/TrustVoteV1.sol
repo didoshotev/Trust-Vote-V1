0f3c4d82f6d385c21e67eaec4401d50d93dd14e1
didoshotev <didoshotev@gmail.com>
2023-05-03 17:30:50 +0300

    feat: update get poll details

--- contracts/TrustVoteV1.sol
+++ contracts/TrustVoteV1.sol
@@ -4,14 +4,12 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "hardhat/console.sol";
 
-
 contract TrustVoteV1 {
     struct Option {
         uint256 id;
         uint256 count;
         string name;
     }
-
 
     struct Poll {
         uint256 id;
@@ -22,7 +20,7 @@ contract TrustVoteV1 {
         bool isActive;
         uint256[] optionIds;
         mapping(uint256 => Option) options;
-        mapping(address => bool) voted;
+        mapping(address => bool) votes;
     }
 
     mapping(uint256 => Poll) polls;
@@ -34,23 +32,38 @@ contract TrustVoteV1 {
     event VoteSuccess(uint256 pollId, uint256 optionId, address voter);
 
     modifier onlyBeforeStart(uint256 pollId) {
-        require(block.timestamp < polls[pollId].startTime, "This action can only be performed before the start time.");
+        require(
+            block.timestamp < polls[pollId].startTime,
+            "This action can only be performed before the start time."
+        );
         _;
     }
 
     modifier onlyBeforeEnd(uint256 pollId) {
-        require(block.timestamp < polls[pollId].endTime, "This action can only be performed before the end time.");
+        require(
+            block.timestamp < polls[pollId].endTime,
+            "This action can only be performed before the end time."
+        );
         _;
     }
 
     modifier onlyPollAdmin(uint256 pollId) {
-        require(msg.sender == polls[pollId].admin, "Only poll admin can perform this action");
+        require(
+            msg.sender == polls[pollId].admin,
+            "Only poll admin can perform this action"
+        );
         _;
     }
 
-
-    function createPoll(string memory name, uint256 startTime, uint256 endTime) public {
-        require(startTime < endTime, "End time should be greater than start time.");
+    function createPoll(
+        string memory name,
+        uint256 startTime,
+        uint256 endTime
+    ) public {
+        require(
+            startTime < endTime,
+            "End time should be greater than start time."
+        );
 
         polls[nextPollId].id = nextPollId;
         polls[nextPollId].admin = msg.sender;
@@ -63,9 +76,18 @@ contract TrustVoteV1 {
         nextPollId++;
     }
 
-    function addOption(uint256 pollId, string memory optionName) public onlyPollAdmin(pollId) onlyBeforeEnd(pollId) {
-        require(msg.sender == polls[pollId].admin, "Only poll admin can add options");
-        require(polls[pollId].id == pollId, "Poll with the given ID does not exist");
+    function addOption(
+        uint256 pollId,
+        string memory optionName
+    ) public onlyPollAdmin(pollId) onlyBeforeEnd(pollId) {
+        require(
+            msg.sender == polls[pollId].admin,
+            "Only poll admin can add options"
+        );
+        require(
+            polls[pollId].id == pollId,
+            "Poll with the given ID does not exist"
+        );
 
         uint optionId = polls[pollId].optionIds.length;
         polls[pollId].optionIds.push(optionId);
@@ -76,22 +98,40 @@ contract TrustVoteV1 {
         });
     }
 
-    function vote(uint256 pollId, uint256 optionId) public { 
-        require(block.timestamp >= polls[pollId].startTime, "The poll has not started yet");
+    function vote(uint256 pollId, uint256 optionId) public {
+        require(
+            block.timestamp >= polls[pollId].startTime,
+            "The poll has not started yet"
+        );
         require(block.timestamp <= polls[pollId].endTime, "The poll has ended");
-        require(!polls[pollId].voted[msg.sender], "You have already voted in this poll");
+        require(
+            !polls[pollId].votes[msg.sender],
+            "You have already votes in this poll"
+        );
         require(optionId < polls[pollId].optionIds.length, "Invalid option ID");
-        require(polls[pollId].id == pollId, "Poll with the given ID does not exist");
-        
+        require(
+            polls[pollId].id == pollId,
+            "Poll with the given ID does not exist"
+        );
+
         polls[pollId].options[optionId].count++;
-        polls[pollId].voted[msg.sender] = true;
+        polls[pollId].votes[msg.sender] = true;
 
         emit VoteSuccess(pollId, optionId, msg.sender);
     }
 
-    function getPollResult(uint256 pollId) public view returns (uint256[] memory counts, string[] memory optionNames) {
-        require(polls[pollId].id == pollId, "Poll with the given ID does not exist");
-        
+    function getPollResult(
+        uint256 pollId
+    )
+        public
+        view
+        returns (uint256[] memory counts, string[] memory optionNames)
+    {
+        require(
+            polls[pollId].id == pollId,
+            "Poll with the given ID does not exist"
+        );
+
         uint256 optionCount = polls[pollId].optionIds.length;
         counts = new uint256[](optionCount);
         optionNames = new string[](optionCount);
@@ -103,12 +143,24 @@ contract TrustVoteV1 {
         }
     }
 
-    function getAllPollIds() public view returns (uint256[] memory) { 
+    function getAllPollIds() public view returns (uint256[] memory) {
         return pollIds;
     }
 
-    function getPollDetails(uint256 pollId) public view returns(string memory name, uint256 startTime, uint256 endTime) {
-        require(polls[pollId].id == pollId, "Poll with the given ID does not exist");
-        return (polls[pollId].name, polls[pollId].startTime, polls[pollId].endTime);
+    function getPollDetails(
+        uint256 pollId
+    ) public view returns (uint256[] memory, uint256[] memory, string memory name, uint256 startTime, uint256 endTime, bool isActive, address admin) {
+        Poll storage poll = polls[pollId];
+
+        uint256[] memory optionIds = new uint256[](poll.optionIds.length);
+        uint256[] memory voteCounts = new uint256[](poll.optionIds.length);
+
+        for (uint256 i = 0; i < poll.optionIds.length; i++) {
+            Option storage option = poll.options[poll.optionIds[i]];
+            optionIds[i] = option.id;
+            voteCounts[i] = option.count;
+        }
+
+        return (optionIds, voteCounts, poll.name, poll.startTime, poll.endTime, poll.isActive, poll.admin);
     }
 }
